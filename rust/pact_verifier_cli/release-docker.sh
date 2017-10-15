7a504b369676ca275f385fd315aee94beb65210a
Assaf Katz <assaf.katz@amdocs.com>
2017-10-15 14:51:50 +0300

    Enhance release-docker.sh files - on behalf of Amdocs Development Limited

--- rust/pact_verifier_cli/release-docker.sh
+++ rust/pact_verifier_cli/release-docker.sh
@@ -2,14 +2,31 @@
 #Copyright: Amdocs Development Limited, 2017
 set -e
 cd `dirname $0`
+
+#Handle proxy
 dockerenv=""
 if [ -n "$HTTPS_PROXY" ]
 then
    dockerenv+="-e HTTPS_PROXY=$HTTPS_PROXY"
 fi
+
+#Initialize parameters
 currdir=$(basename $(pwd))
 uidgid=$(stat -c "%u:%g" .)
 GID=$(id -g)
-docker run --rm $dockerenv -it -v "$(pwd)/..":/rust -u rust:rust --group-add $GID --group-add sudo -w /rust/$currdir ekidd/rust-musl-builder bash -c "cargo build --release && sudo chown -R $uidgid target"
-docker build -t $currdir .
+version=$(sed -rn '/\[package\]/,/\[dependencies\]/{s/^version = "([^"]+)"/\1/p}' Cargo.toml)
+
+#Build the release in alphine enviornmnet
+docker run --rm $dockerenv -it -v "$(pwd)/..":/rust -u rust:rust --group-add $GID --group-add sudo -w /rust/$currdir ekidd/rust-musl-builder \
+       bash -c "cargo build --release && sudo chown -R $uidgid target"
+
+#Create the image
+docker build -t $currdir:$version .
+
+#Push to repostiory
+#export DOCKER_ID_USER="assafkatz3"
+#docker tag $currdir:$version docker.io/$DOCKER_ID_USER/$currdir:$version
+#docker push docker.io/$DOCKER_ID_USER/$currdir:$version
+#docker tag $currdir:$version docker.io/$DOCKER_ID_USER/$currdir
+#docker push docker.io/$DOCKER_ID_USER/$currdir
 
