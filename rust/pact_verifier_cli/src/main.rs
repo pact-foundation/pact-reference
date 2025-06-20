ce3c5d9eab8b3a72b120851553c9f30a6149442c
Ronald Holshausen <ronald.holshausen@smartbear.com>
2025-06-20 11:56:43 +1000

    feat(pact_verifier_cli): Update the docs on the new options flag #494

--- rust/pact_verifier_cli/src/main.rs
+++ rust/pact_verifier_cli/src/main.rs
@@ -31,7 +31,7 @@
 //!       --compact-log          Emit logs optimized for short line lengths.
 //!   -j, --json <json-file>     Generate a JSON report of the verification [env: PACT_VERIFIER_JSON_REPORT=]
 //!   -x, --junit <junit-file>   Generate a JUnit XML report of the verification (requires the junit feature) [env: PACT_VERIFIER_JUNIT_REPORT=]
-//!       --no-colour            Disables ANSI escape codes in the output [aliases: no-color]
+//!       --no-colour            Disables ANSI escape codes in the output [aliases: --no-color]
 //!
 //! Loading pacts options:
 //!   -f, --file <file>
@@ -110,7 +110,11 @@
 //!       --enable-pending
 //!           Enables Pending Pacts
 //!       --include-wip-pacts-since <include-wip-pacts-since>
-//!           Allow pacts that don't match given consumer selectors (or tags) to  be verified, without causing the overall task to fail. For more information, see https://pact.io/wip
+//!           Allow pacts that don't match given consumer selectors (or tags) to  be verified, without causing the overall task to fail. For more information, see https://!pact.io/wip
+//!
+//! Development options:
+//!       --exit-on-first-error  Stops the verifier at the first failure
+//!       --last-failed          Only runs the interactions that failed on the previous verifier run. Requires --json-file to have been set
 //! ```
 //!
 //! ## Options
@@ -224,6 +228,21 @@
 //! ```sh
 //! --consumer-version-selectors '{"branch": "master"}'
 //! ```
+//!
+//! ### Development options
+//!
+//! There are two options that support filtering interactions to help with setting up your Pact tests. For instance, setting
+//! up provider state can be quite involved.
+//!
+//! #### Exit on first error
+//!
+//! The `--exit-on-first-error` option will terminate the verification run on the first error, and will skip all remaining interactions
+//! and Pact files.
+//!
+//! ### Only run previously failed interactions
+//!
+//! The `--last-failed` option will only execute interactions that have previously failed. It requires the `--json` option
+//! (the previous state will be loaded from this file).
 //!
 //! ## Example run
 //!
