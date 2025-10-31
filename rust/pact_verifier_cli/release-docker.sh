9952456cc9047ae58ecd27591032e08ecc0490c8
Yousaf Nabi <yousaf@pactflow.io>
2025-10-31 18:51:24 +0000

    chore(docker): ghcr publish [ci skip]

--- rust/pact_verifier_cli/release-docker.sh
+++ rust/pact_verifier_cli/release-docker.sh
@@ -27,3 +27,17 @@ docker manifest create pactfoundation/pact-verifier:latest \
     --amend pactfoundation/pact-verifier:$1-amd64 \
     --amend pactfoundation/pact-verifier:$1-arm64v8
 docker manifest push pactfoundation/pact-verifier:latest
+
+# publish to ghcr, pactfoundation must be renamed to pact-foundation
+docker tag pactfoundation/pact-verifier:$1-amd64 ghcr.io/pact-foundation/pact-verifier:$1-amd64
+docker push ghcr.io/pact-foundation/pact-verifier:$1-amd64
+docker tag pactfoundation/pact-verifier:$1-arm64v8 ghcr.io/pact-foundation/pact-verifier:$1-arm64v8
+docker push ghcr.io/pact-foundation/pact-verifier:$1-arm64v8
+docker manifest create ghcr.io/pact-foundation/pact-verifier:$1 \
+    --amend ghcr.io/pact-foundation/pact-verifier:$1-amd64 \
+    --amend ghcr.io/pact-foundation/pact-verifier:$1-arm64v8
+docker manifest push ghcr.io/pact-foundation/pact-verifier:$1
+docker manifest create ghcr.io/pact-foundation/pact-verifier:latest \
+    --amend ghcr.io/pact-foundation/pact-verifier:$1-amd64 \
+    --amend ghcr.io/pact-foundation/pact-verifier:$1-arm64v8
+docker manifest push ghcr.io/pact-foundation/pact-verifier:latest
